ae76218d1d3a6bc25a637ae9b7c723c96db09ef1
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-17 10:59:02 -0300

    refactor: move sidebar styles to a separate file

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,4 @@
-import { styled } from '@stitches/react';
-import { Link } from "react-router-dom";
+import { Nav, SidebarLink, SidebarLinks, SidebarLogo, SidebarMain } from "./styles"
 
 import logoSvg from "../../assets/logo.svg"
 import homeSvg from "../../assets/icons/sidebar/home.svg"
@@ -9,87 +8,6 @@ import associatesSvg from "../../assets/icons/sidebar/UsersFour.svg"
 import userSvg from "../../assets/icons/sidebar/user.svg"
 import configSvg from "../../assets/icons/sidebar/GearSix.svg"
 import arrowSvg from "../../assets/icons/sidebar/CaretDown.svg"
-
-
-const SidebarMain = styled('aside', {
-  width: '300px',
-  height: '100vh',
-  position: 'fixed',
-  left: 0,
-  top: 0,
-  zIndex: 1000,
-  background: '#FFFFFF',
-
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-})
-
-const SidebarLogo = styled('div', {
-  width: '100%',
-  height: '150px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-
-  marginBottom: '20px',
-
-  '> img': {
-    width: '180px'
-  }
-});
-
-const Nav = styled('nav', {
-  width: '100%',
-  marginLeft: '100px',
-})
-
-const SidebarLinks = styled('div', {
-  width: '200px',
-
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '50px',
-
-  alignItems: 'center',
-});
-
-
-
-const SidebarLink = styled(Link, {
-  width: '100%',
-
-  display: 'flex',
-  alignItems: 'center',
-  gap: '10px',
-  
-  textDecoration: 'none',
-  color: "#121929",
-  
-  fontSize: '18px',
-  fontWeight: '500',
-  lineHeight: '14.63px',
-
-  '> img': {
-    width: '25px',
-    height: '25px',
-    marginBottom: '3px',
-
-    '&.arrow': {
-      width: '15px',
-      height: '15px',
-      marginLeft: '15px',
-      marginBottom: '3px'
-    }
-  },
-
-  '&.active': {
-    background: "#D0005E",
-    padding: "14px",
-    color: "#FFFFFF",
-    borderRadius: "24px"
-  },
-});
 
 export const Sidebar = () => {
   return (
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,1 +1,80 @@
-import { globalCss } from '@stitches/react';
+import { styled } from '@stitches/react';
+import { Link } from "react-router-dom";
+
+export const SidebarMain = styled('aside', {
+  width: '300px',
+  height: '100vh',
+  position: 'fixed',
+  left: 0,
+  top: 0,
+  zIndex: 1000,
+  background: '#FFFFFF',
+
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
+
+export const SidebarLogo = styled('div', {
+  width: '100%',
+  height: '150px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  marginBottom: '20px',
+
+  '> img': {
+    width: '180px'
+  }
+});
+
+export const Nav = styled('nav', {
+  width: '100%',
+  marginLeft: '100px',
+})
+
+export const SidebarLinks = styled('div', {
+  width: '200px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '50px',
+
+  alignItems: 'center',
+});
+
+export const SidebarLink = styled(Link, {
+  width: '100%',
+
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  
+  textDecoration: 'none',
+  color: "#121929",
+  
+  fontSize: '18px',
+  fontWeight: '500',
+  lineHeight: '14.63px',
+
+  '> img': {
+    width: '25px',
+    height: '25px',
+    marginBottom: '3px',
+
+    '&.arrow': {
+      width: '15px',
+      height: '15px',
+      marginLeft: '15px',
+      marginBottom: '3px'
+    }
+  },
+
+  '&.active': {
+    background: "#D0005E",
+    padding: "14px",
+    color: "#FFFFFF",
+    borderRadius: "24px"
+  },
+});
